0ed23211c14a56f699471c7d3acfbcfb5750f663
Joshua Boss <joshua.boss@duke.edu>
2020-07-12 16:13:39 -0400

    Add existing file

---
+++ jsonRead.py
@@ -0,0 +1,34 @@
+import json
+import csv
+
+
+def parser(filename):
+    list = []
+    retList = []
+
+    for line in open(filename, 'r'):
+        list.append(json.loads(line))
+
+    for L in list:
+        dict = {}
+        dict["iteration"] = L["iteration"]
+        dict["total_loss"] = L["total_loss"]
+        retList.append(dict)
+
+    return retList
+
+def CSVwrite(listDicts):
+    toCSV = listDicts
+    print(toCSV[0])
+   
+    keys = toCSV[0].keys()
+    
+    with open('trainMetrics.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(toCSV)
+
+if __name__ == "__main__":
+    
+    targetList = (parser('metrics.json')) 
+    CSVwrite(targetList)
